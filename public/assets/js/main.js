e171102c4ce3f482e0dc09609484f532f552c26d
samer.bahri98 <samer.bahri@ieee.org>
2020-06-30 19:24:05 +0200

    feat:firebase

---
+++ public/assets/js/main.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyAejIml3d2A5jZXNGQk-w63w3_ssnjpHPs",
+  authDomain: "samer-bahri.firebaseapp.com",
+  databaseURL: "https://samer-bahri.firebaseio.com",
+  projectId: "samer-bahri",
+  storageBucket: "samer-bahri.appspot.com",
+  messagingSenderId: "739600750651",
+  appId: "1:739600750651:web:bc712c30c7d4125aada41e",
+  measurementId: "G-3SKMXZTX17",
+};
+
+
