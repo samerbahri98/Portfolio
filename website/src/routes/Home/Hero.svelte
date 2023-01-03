30b5726c39a7d452c408145908ce12eca1feebf8
samerbahri98 <samer.bahri@thebahrimedia.com>
2023-01-03 02:53:16 +0100

    fix: hero padding

--- website/src/routes/Home/Hero.svelte
+++ website/src/routes/Home/Hero.svelte
@@ -1,4 +1,4 @@
-<section class="pt-10 overflow-hidden bg-gray-50 md:pt-0 sm:pt-16 2xl:pt-16">
+<section class="pt-10 overflow-hidden bg-gray-50 md:pt-0 sm:pt-16 2xl:pt-16 py-10">
     <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div class="grid items-center grid-cols-1 md:grid-cols-2">
             <div>
